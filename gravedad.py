be036bdbe6032eba3fbf10e0e93867bb40e875fd
Javi Sala <javisala.ca@gmail.com>
2020-02-04 11:54:29 -0300

    Practica de fisica

--- gravedad.py
+++ gravedad.py
@@ -1,32 +1,15 @@
-"""sacado de https://pymunk-tutorial.readthedocs.io/en/latest/intro/intro.html#a-bouncing-ball"""
-
-
-from recursos import colores
-import pymunk
-import pymunk.pygame_util
-import pygame
-pygame.init()
-size = 640, 240
-screen = pygame.display.set_mode(size)
-draw_options = pymunk.pygame_util.DrawOptions(screen)
-space = pymunk.Space()
-space.gravity = 0, -900
-b0 = space.static_body
-segment = pymunk.Segment(b0, (0, 0), (640, 0), 4)
-segment.elasticity = 1
+from intro import pymunk, space, App
 body = pymunk.Body(mass=1, moment=10)
-body.position = 100, 200
-circle = pymunk.Circle(body, radius=20)
-circle.elasticity = 0.95
-space.add(body, circle, segment)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill(colores.Gray)
-    space.debug_draw(draw_options)
-    pygame.display.update()
-    space.step(0.01)
+body.position = (100, 200)
 
-pygame.quit()
+segment = pymunk.Segment(space.static_body, (20, 120), (400, 20), 1)
+segment.elasticity = 0.5
+segment.friction = 0.5
+circle = pymunk.Circle(body, radius=20)
+circle.elasticty = 1
+circle.friction = 0.5
+
+space.add(body, circle, segment)
+
+
+App().run()
